c0f0198086cb3c1b953d75b95691e65d48ff5745
kirillov.o.a <kirillov.o.a@ya.ru>
2017-12-18 02:32:36 +0400

    gen docs to matrix_multiplication.cpp

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main',['main',['../db/d14/group__trivial__c__group.html#ga0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;trivial.c'],['../d4/d66/group__trivial__cpp__group.html#gae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;trivial.cpp']]]
+  ['main',['main',['../d9/d13/group__matrix__multiplication.html#gae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;matrix_multiplication.cpp'],['../db/d14/group__trivial__c__group.html#ga0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;trivial.c'],['../d4/d66/group__trivial__cpp__group.html#gae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;trivial.cpp']]],
+  ['multiplymatrices',['multiplyMatrices',['../d9/d13/group__matrix__multiplication.html#ga299d3b87a6d100b7c1f7c0b4771daf4d',1,'matrix_multiplication.cpp']]]
 ];
