1f5713f5ab82ec1f1d0a6d3c92202c35618544e8
kirillov.o.a <kirillov.o.a@ya.ru>
2017-12-18 08:08:34 +0400

    add speach to docs

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['programmingtechnologiesopencl',['ProgrammingTechnologiesOpenCL',['../d3/dcc/md__r_e_a_d_m_e.html',1,'']]]
+  ['programmingtechnologiesopencl',['ProgrammingTechnologiesOpenCL',['../index.html',1,'']]]
 ];
